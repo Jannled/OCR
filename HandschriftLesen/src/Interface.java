2380f900428c892de7b47cd8cede90bfa3c4da96
alle <alle@IFR2-05.convos.local>
2017-06-14 15:13:56 +0200

    Habe Fertig

---
+++ HandschriftLesen/src/Interface.java
@@ -0,0 +1,68 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Interface extends JFrame {
+
+	private JPanel contentPane;
+	JButton feld[][] = new JButton[5][6];
+	int felder[] = new int[30];
+	int feldI[] = new int[30];
+	boolean feldB[] = new boolean[30];
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface frame = new Interface();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Interface() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1080, 720);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Willkommen bei Janniks und Noahs KI");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 25));
+		lblNewLabel.setBounds(100, 50, 500, 75);
+		contentPane.add(lblNewLabel);
+		
+		JButton b00 = new JButton("");
+		b00.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		b00.setBounds(100, 150, 30, 30);
+		contentPane.add(b00);
+		
+		
+		
+	}
+}
